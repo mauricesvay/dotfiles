a52d7131f72c7bb403a38af0aed5f14eba32bf58
Maurice Svay <maurice@sketchfab.com>
2014-07-25 09:59:38 +0200

    Add ios alias

--- system/aliases.sh
+++ system/aliases.sh
@@ -1,3 +1,4 @@
 alias ls='ls -G'
 alias ll='ls -lG'
 alias la='ls -AG'
+alias ios='open /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/Applications/iPhone\ Simulator.app'
